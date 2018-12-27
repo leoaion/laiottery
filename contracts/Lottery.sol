0c8e12b0532c54579a57812fc9ff903c875fb4af
Leo Ren <leo.ren@aion.network>
2018-12-27 10:20:48 +0800

    init.

---
+++ contracts/Lottery.sol
@@ -0,0 +1,67 @@
+pragma solidity 0.4.15;
+
+
+contract Lottery {
+    string private name;
+    uint8 private count;
+    string private desc;
+    address private initiator;
+    address[] private participants;
+    bool private isWaiting = false;
+
+    event Winner(uint128 time, string lotteryName, address[] winners);
+
+    function initiate(string _name, uint8 _count, string _desc) public {
+        require(isWaiting == false);
+        require(_count > 0);
+
+        name = _name;
+        count = _count;
+        desc = _desc;
+        initiator = msg.sender;
+        isWaiting = true;
+
+    }
+
+    function participate() public {
+        require(isWaiting == true);
+        require(isAddressIncluded(msg.sender, participants) == false);
+        participants.push(msg.sender);
+    }
+
+    function draw() public {
+        require(participants.length > 0);
+        require(msg.sender == initiator);
+        address[] memory winners = new address[](count);
+
+        for (uint i = 0; i < count; i++) {
+            uint index = winnerNumber();
+            address winner = participants[index];
+            if (isAddressIncluded(winner, winners)) {
+                i--;
+            } else {
+                winners[i] = winner;
+            }
+        }
+        Winner(block.timestamp, name, winners);
+
+        delete participants;
+        count = 0;
+        isWaiting = false;
+    }
+
+    function isAddressIncluded(address one, address[] all) private returns(bool) {
+        for (uint i = 0; i < all.length; i++) {
+            if (all[i] == one) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function winnerNumber() private returns(uint) {
+        return uint(blake2b256(block.timestamp)) % participants.length;
+    }
+
+
+}
---
+++ titanrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  defaultBlockchain: "aion",
+  blockchains: {
+    aion: {
+      networks: {
+        development: {
+          host: "http://127.0.0.1:8645",
+          defaultAccount: "0xa05ed4fcb3fd1c2b8d65f7a9cbff0e280e53b40e6399f9887c3e28b37b5d09bf",
+          password: "password"
+        }
+      }
+    }
+  }
+};
